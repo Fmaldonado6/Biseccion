cb3ca4bfacdcdc2d0014f861fdb0d5c6e72c7bc6
Fernando Maldonado <f_maldonado6@hotmail.com>
2021-09-05 18:40:18 -0500

    Bug fixes

--- js/index.js
+++ js/index.js
@@ -6,10 +6,15 @@ const errorInput = document.getElementById("error");
 const variable = "x";
 const symbols = new Set(["+", "-", "*", "/", "(", ")"]);
 
+const replace = { "^": "**", e: Math.E };
+
 function solve() {
   let x1 = x1Input.value;
   let xu = xuInput.value;
   let equation = equationInput.value;
+
+  if (!equation) throw Error("Ingresa una ecuacion");
+  if (!x1 || !xu) throw Error("Ingresa valores en el rango");
   evalEquation(equation, x1);
 }
 
@@ -17,12 +22,15 @@ function evalEquation(equation, x) {
   let firstChar = true;
 
   let formattedEquation = "";
+  let previous;
 
   for (let char of equation) {
+    if (replace[char]) char = replace[char];
     if (!firstChar && char == variable) formattedEquation += "*" + x;
     else if (char == variable) formattedEquation += x;
     else formattedEquation += char;
     firstChar = symbols.has(char);
+    previous = char;
   }
 
   console.log(formattedEquation);
