2bbc344a25566df2c94eee2532f1bb29a3544d02
Kevin Villalobos <kevin.villalobosvl@outlook.com>
2021-09-07 19:31:27 -0500

    results mejorado

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const equationInput = document.getElementById("equation");
 const x1Input = document.getElementById("x1");
 const xuInput = document.getElementById("xu");
+const stepsCard = document.getElementById("steps-card");
+const steps = document.getElementById("steps");
 const resultCard = document.getElementById("result-card");
 const results = document.getElementById("results");
 const errorInput = document.getElementById("error");
@@ -23,7 +25,7 @@ function solve() {
     //por eso valida que no sea cero ni positivo el resultado de multiplicar
     if ((evalEquation(equation, x1))*(evalEquation(equation, xu))>=0) throw Error("El intervalo no contiene a la solucion, intenta otro")
     if (error === null) throw Error("Ingresa el error");
-    
+    steps.innerHTML = "";
     results.innerHTML = "";
 
     let absError = error + 1;
@@ -34,32 +36,36 @@ function solve() {
       xr = (x1 + xu) / 2;
       
       if(lastAprox)
-      absError = absoluteError(xr, lastAprox);
+      absError = absoluteError(lastAprox, xr);
       
       const f1 = evalEquation(equation, x1);
       const fr = evalEquation(equation, xr);
-      const fu = evalEquation(equation,xu);
 
       const f1fr = f1 * fr;
       const element = document.createElement("p");
-      element.innerHTML = `X<sub>1</sub> = ${x1}, X<sub>u</sub> = ${
-       xu }<br> X<sub>r</sub> = (${x1} + ${xu}) / 2 <br>
-       X<sub>r</sub> = ${xr}<br>
-       F(${x1})*F(${xr})= ${f1} * ${fu} 
-       = ${f1fr} ${f1fr > 0 ? " >0<br> X<sub>1</sub> = X<sub>r</sub><br>" : " <0<br>X<sub>u</sub> = X<sub>r</sub><br>"}<br>
-       ${lastAprox ? `E<sub>a</sub> = | ${xr} - ${lastAprox} / ${xr} | (100%) = ${absError}<br>` : "<br>"}`;
+      element.innerHTML = `X<sub>1</sub> = ${x1.toFixed(4)}, X<sub>u</sub> = ${
+       xu.toFixed(4) }<br> X<sub>r</sub> = (${x1.toFixed(4)} + ${xu.toFixed(4)}) / 2 = ${xr.toFixed(4)}<br>
+       F( ${x1.toFixed(4)} ) * F( ${xr.toFixed(4)} )= ${f1.toFixed(4)} * ${fr.toFixed(4)} 
+       = ${f1fr.toFixed(4)} ${f1fr > 0 ? " > 0<br> X<sub>1</sub> = X<sub>r</sub><br>" : " < 0<br>X<sub>u</sub> = X<sub>r</sub><br>"}<br>
+       ${lastAprox ? `E<sub>a</sub> = | ${xr.toFixed(4)} - ${lastAprox.toFixed(4)} / ${xr.toFixed(4)} | (100%) = ${absError % 1 == 0 ? absError : absError.toFixed(4)}% <br>` : "<br>"}`;
   
-      results.appendChild(element);
+      steps.appendChild(element);
       if (f1fr == 0) break;
       else if (f1fr > 0) x1 = xr;
       else xu = xr;
-
+      
       
       lastAprox = xr;
       console.log(absError, error);
     }
 
     console.log("Valor aproximado: " + xr, "Error: ", absError);
+    const element = document.createElement("p");
+    element.innerHTML = `Valor aproximado: ${xr}, Error aproximado: ${absError.toFixed(4)}`;
+    results.appendChild(element);
+    
+    showResultCard();
+
   } catch (e) {
     showSnackbar(e);
   }
@@ -67,6 +73,24 @@ function solve() {
 
 function absoluteError(x1, x2) {
   return Math.abs((x2 - x1) / x2) * 100;
+}
+
+function showResultCard() {
+  if (resultCard.classList.contains("hidden"))
+    resultCard.classList.remove("hidden");
+  resultCard.classList.add("show");
+  if (stepsCard.classList.contains("hidden"))
+    stepsCard.classList.remove("hidden");
+  stepsCard.classList.add("show");
+}
+
+function hideResultCard() {
+  if (resultCard.classList.contains("show"))
+    resultCard.classList.remove("show");
+  resultCard.classList.add("hidden");
+  if (stepsCard.classList.contains("show"))
+    stepsCard.classList.remove("show");
+  stepsCard.classList.add("hidden");
 }
 
 function evalEquation(equation, x) {
